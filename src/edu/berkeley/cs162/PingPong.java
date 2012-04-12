98f1773162e645405b8c52d49145af7d93cd0013
Jacob Rashoff <jacobrashoff@berkeley.edu>
2012-04-11 20:54:54 -0700

    PingPong updated

--- src/edu/berkeley/cs162/PingPong.java
+++ src/edu/berkeley/cs162/PingPong.java
@@ -9,14 +9,15 @@ public class PingPong {
 	 */
 	public static void main(String[] args) throws IOException {
 		ServerSocket s = new ServerSocket(8081, 20);
-		Socket s1 = s.accept();
-		OutputStream s1out = s1.getOutputStream();
-		DataOutputStream dos = new DataOutputStream(s1out);
-		dos.writeUTF("pong");
-		dos.close();
-		s1out.close();
-		s1.close();
-
+		while(true){
+			Socket s1 = s.accept();
+			OutputStream s1out = s1.getOutputStream();
+			DataOutputStream dos = new DataOutputStream(s1out);
+			dos.writeUTF("pong");
+			dos.close();
+			s1out.close();
+			s1.close();
+		}	
 	}
 
 }
