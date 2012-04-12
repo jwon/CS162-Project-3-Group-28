fb1d56c193c6310f948d339de141d03f0d7d9010
Jacob Rashoff <jacobrashoff@berkeley.edu>
2012-04-11 22:24:06 -0700

    PingPong using threads

--- src/edu/berkeley/cs162/PingPong.java
+++ src/edu/berkeley/cs162/PingPong.java
@@ -1,23 +1,59 @@
 import java.net.*;
+import java.lang.*;
 import java.io.*;
 
 public class PingPong {
 
+	
+	private ServerSocket s;
+	private int port = 8081;
+	
+	public PingPong(){
+		try {
+			s = new ServerSocket(port);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void handleConnection(){
+		while(true){
+			try{
+				Socket socket = s.accept();
+				new ConnectionHandler(socket);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws IOException {
-		ServerSocket s = new ServerSocket(8081, 20);
-		while(true){
-			Socket s1 = s.accept();
-			OutputStream s1out = s1.getOutputStream();
-			DataOutputStream dos = new DataOutputStream(s1out);
-			dos.writeUTF("pong");
-			dos.close();
-			s1out.close();
-			s1.close();
-		}	
+	public static void main(String[] args){
+		PingPong test = new PingPong();
+		test.handleConnection();
 	}
 
+	public class ConnectionHandler implements Runnable {
+		
+		private Socket s1;
+		
+		public ConnectionHandler(Socket socket){
+			this.s1 = socket;
+			Thread t = new Thread(this);
+			t.start();
+		}
+		
+		public void run(){
+			try{
+				ObjectOutputStream oos = new ObjectOutputStream(s1.getOutputStream());
+				oos.writeObject("pong");
+				oos.close();
+				s1.close();
+			} catch (IOException e){
+				e.printStackTrace();
+			}
+		}
+	}
 }
