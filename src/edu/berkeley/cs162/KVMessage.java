a98a69987c0af55e1daa8ec191d0a3fba9cdc231
James Won <james.won@berkeley.edu>
2012-04-16 00:23:06 -0700

    changing marshal and unmarshal

--- src/edu/berkeley/cs162/KVMessage.java
+++ src/edu/berkeley/cs162/KVMessage.java
@@ -57,6 +57,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.Text;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 
 /**
