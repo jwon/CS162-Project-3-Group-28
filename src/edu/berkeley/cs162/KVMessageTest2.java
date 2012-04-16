cb6ecbb1d9919bdfc9647e402f207e855180e2dc
James Won <james.won@berkeley.edu>
2012-04-15 23:45:03 -0700

    KVMessageTest2

---
+++ src/edu/berkeley/cs162/KVMessageTest2.java
@@ -0,0 +1,21 @@
+package edu.berkeley.cs162;
+
+import static org.junit.Assert.*;
+import java.io.Serializable;
+import org.junit.Test;
+
+public class KVMessageStringTest {
+    
+    public static void runTest() {
+
+	    Key key = new Key("key");
+	    Value value = new Value("value");
+	    KVMessage msg = new KVMessage("putreq", KVMessage.marshal(key), KVMessage.marshal(value));
+
+	    System.out.println(msg.toXML());
+    }
+
+    public static void main(String args[]) {
+	runTest();
+    }
+}
