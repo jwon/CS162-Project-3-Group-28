50b1c5c8f48b28fb59fb0b8617c0087e1df97f9b
James Won <james.won@berkeley.edu>
2012-04-17 00:21:31 -0700

    KVServer debug

--- src/edu/berkeley/cs162/KeyServer.java
+++ src/edu/berkeley/cs162/KeyServer.java
@@ -73,7 +73,7 @@ public class KeyServer<K extends Serializable, V extends Serializable> implement
 	    boolean store = dataStore.put(key,value);
 	    boolean cache = dataCache.put(key,value);
 		
-		System.out.println("dataStore.get: " + dataStore.get(key);
+		System.out.println("dataStore.get: " + dataStore.get(key));
 		
 		System.out.println("store: " + store);
 		System.out.println("cache: " + cache);
