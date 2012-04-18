3e1410049da6edf77236a811c9c0470c4b629a6f
James Won <james.won@berkeley.edu>
2012-04-17 22:50:06 -0700

    debugging statements

--- src/edu/berkeley/cs162/KVClientTest.java
+++ src/edu/berkeley/cs162/KVClientTest.java
@@ -73,7 +73,7 @@ public class KVClientTest {
 
 		//Test get
 		System.out.println("Testing GET");
-		System.out.println("Get HI: " + kvc.get("HI"));
+		System.out.println("Get HI: " + kvc.get((Key)"HI"));
 		
 		if(v1.equals(kvc.get(k1))){
 			System.out.println("GET SUCCEEDED.");
