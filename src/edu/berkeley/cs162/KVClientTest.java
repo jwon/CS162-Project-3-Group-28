fd1f8aa87488d037e2fa7ad57331133d0543ab66
James Won <james.won@berkeley.edu>
2012-04-17 23:50:07 -0700

    potential fix for clienthandler

--- src/edu/berkeley/cs162/KVClientTest.java
+++ src/edu/berkeley/cs162/KVClientTest.java
@@ -75,7 +75,7 @@ public class KVClientTest {
 		System.out.println("Testing GET");
 		
 		//Value.equals(Value)
-		System.out.println("kvc.get(k1): " + kvc.get(k1););
+		System.out.println("kvc.get(k1): " + kvc.get(k1));
 		/*
 		if(v1.equals(temp)){
 			System.out.println("GET SUCCEEDED.");
